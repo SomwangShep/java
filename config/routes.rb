a9d9f06b5c669290417708fc5ab73bdc61fa1868
SomwangShep <ss1792@mcla.edu>
2017-06-25 02:04:17 +0000

    Second comit

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,19 @@ end
   root "posts#index"
   
   get '/about', to: 'pages#about'
+  get '/chapter1', to: 'pages#chapter1'
+  get '/chapter2', to: 'pages#chapter2'
+  get '/chapter3', to: 'pages#chapter3'
+  get '/chapter4', to: 'pages#chapter4'
+  get '/chapter5', to: 'pages#chapter5'
+  get '/chapter6', to: 'pages#chapter6'
+  get '/chapter7', to: 'pages#chapter7'
+  get '/chapter8', to: 'pages#chapter8'
+  get '/chapter9', to: 'pages#chapter9'
+  get '/chapter10', to: 'pages#chapter10'
+  get '/chapter11', to: 'pages#chapter11'
+  get '/chapter12', to: 'pages#chapter12'
+  get '/chapter13', to: 'pages#chapter13'
+  get '/chapter14', to: 'pages#chapter14'
+  
 end
